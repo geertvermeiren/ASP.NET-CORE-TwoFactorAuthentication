1441af3c59eee6b7d21dcf4b8c2b674cc50c08af
geert vermeiren <gvermeiren@hotmail.com>
2019-12-09 17:08:24 -0200

    intial commit

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using QRCODE_TWOFACTOR.Models;
+
+[assembly: HostingStartup(typeof(QRCODE_TWOFACTOR.Areas.Identity.IdentityHostingStartup))]
+namespace QRCODE_TWOFACTOR.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<QRCODE_TWOFACTORContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("QRCODE_TWOFACTORContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<QRCODE_TWOFACTORContext>();
+            });
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QRCODE_TWOFACTOR.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
